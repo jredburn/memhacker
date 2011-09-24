b33c82a14e58992a4684d2bbb2a5eba0699d411e
Jeremy Redburn <jredburn@gmail.com>
2011-09-24 13:25:43 -0400

    Moved to Rails 3.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Memhacker::Application.config.secret_token = 'ec4c3315d6344ba9777e653802b85feada6f0301635b11f7cc265a08b676a4563ffa6b4f6e630158b8a925ccda328e614cd47618b393efe69cb2384c738b402a'
+Memhacker::Application.config.secret_token = 'e76bb4453e73ae20f70b2e9dc754b9c416a98fa47d566c4b849cda42df01650899be4dd41e236d50c0144e27948b37c15b83fed62f02682e86889b732e8d6972'
