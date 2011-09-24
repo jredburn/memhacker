c954e84fffb01b7dadac4335cb43320dc84941e9
Jeremy Redburn <jredburn@gmail.com>
2011-09-24 11:45:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memhacker::Application.config.secret_token = 'ec4c3315d6344ba9777e653802b85feada6f0301635b11f7cc265a08b676a4563ffa6b4f6e630158b8a925ccda328e614cd47618b393efe69cb2384c738b402a'
