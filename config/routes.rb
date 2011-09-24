cfe9f15f670ae090b51d62f6b020627bb455203b
Jeremy Redburn <jredburn@gmail.com>
2011-09-24 12:46:19 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Memhacker::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
