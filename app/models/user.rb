71d3c5726b39ce59e37760304101ba0524f47af6
Jeremy Redburn <jredburn@gmail.com>
2011-09-24 14:08:00 -0400

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,21 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  has_secure_password
+  
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
+  
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
-
+  
+  validates_presence_of :password, :on => :create
+  
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
 end
 
